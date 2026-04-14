c9e0ef426ec7c036f5187ac66828276111d72d6b
Anas Absi <ansmwmn730@gmail.com>
2026-04-14 20:59:21 +0300

    update settings for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^s^8a+^qez7qgx(ietx*b^13rkfiqonbva-ccfv&g))gd@u-5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
