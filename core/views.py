4fba1cf6209a0ed4338aa0284d433797c912e2c1
Anas Absi <ansmwmn730@gmail.com>
2026-04-14 14:02:09 +0300

    fix project

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
-def homePageview(request):
-	return HttpResponse('HelloWorld')
+from django.views import View
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+	template_name = 'home.html'
+		
+class AboutPageView(TemplateView):
+	template_name = 'about.html'	
+# Create your views here.
+	
